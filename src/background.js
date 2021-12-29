9824e23f0a6d8229b5fd12f61d3c965c0c45b023
D4ker <Alex-Daker@yandex.ru>
2021-12-29 11:34:32 +0300

    Added export method in interface

--- src/background.js
+++ src/background.js
@@ -8,7 +8,7 @@ chrome.tabs.onUpdated.addListener(
     function (tabId, changeInfo, tab) {
         if (changeInfo.url) {
             chrome.tabs.sendMessage(tabId, {
-                message: Constants.UPDATE_TABS_MSG
+                type: Constants.UPDATE_TABS_MSG
             });
         }
     }
--- src/content.js
+++ src/content.js
@@ -1,3 +1,5 @@
 // Расширение для экспорта диалогов из ВК и импорта в Telegram
 
 const Button = require('./js/export/export-button');
+
+Button.init();
